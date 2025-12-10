a5a9e3ca10b48fd76a549cfc11bd0d8feee1319b
ThiLanAnhHa <imhathilananh@gmail.com>
2025-12-09 18:09:58 -0800

    fix(gradle):update kotlin version to match compatibility

--- app/src/main/java/com/zybooks/appmobilefinalproject/MainActivity.kt
+++ app/src/main/java/com/zybooks/appmobilefinalproject/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.tabs.TabLayout
 import android.widget.ImageButton
 import android.content.Intent
 import com.google.android.material.slider.RangeSlider
